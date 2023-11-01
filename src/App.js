5f37160b8fade1ac63c1232b4e92b3c40299a23c
Antonela <antonelaborgogno@gmail.com>
2023-11-01 11:37:29 -0300

    componentes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Siguiente from './components/Siguiente.jsx'
 
 function App() {
   return (
     <div className="App">
-
+      <Siguiente/>
     </div>
   );
 }
---
+++ src/components/Siguiente.jsx
@@ -0,0 +1,35 @@
+import React, {useEffect, useState} from 'react'
+
+const Siguiente = () => {
+    let [pokemonName, setPokemonName] = useState('')
+  let [pokemonImg, setPokemonImg] = useState('')
+  const [id, setId] = useState(1);
+
+  function siguiente() {
+    setId(id + 1);
+  }
+
+
+  useEffect(() => {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+      .then(result => result.json())
+      .then(data => {
+        setPokemonName(data.name);
+        setPokemonImg(data.sprites.front_default);
+      });
+  }, [id]);
+
+
+  return (
+    <>
+      <div className=''>
+        <h1 className=''>{pokemonName.toUpperCase()}</h1>
+        <img src={pokemonImg} alt="" className='' />
+        <button className='' onClick={siguiente}> Siguiente </button>
+        <p>© JSA </p>
+      </div>
+    </>
+  )
+}
+
+export default Siguiente
